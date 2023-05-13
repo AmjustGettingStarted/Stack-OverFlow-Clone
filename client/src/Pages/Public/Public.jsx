b460fa91e44ca8695e4ae48d41c3df3f88676866
AmjustGettingStarted <gamerhmv2050@gmail.com>
2023-05-14 01:05:55 +0530

    chat community update

--- client/src/Pages/Public/Public.jsx
+++ client/src/Pages/Public/Public.jsx
@@ -6,17 +6,10 @@ const Public = () => {
     <div style={{ marginTop:'50px' }}>
   <ChatEngine
    height="100vh"
-<<<<<<< HEAD
    projectID="2d39e73d-5e7c-4417-95dc-2f5d355f3229"
    userName="user1"
    userSecret="user1"
    />    
-=======
-   projectID="2908cefb-f853-471a-a7ba-ee5861afadee"
-   userName="admin"
-   userSecret="admin123"
-   />   
->>>>>>> baac24658afa4ece349bd0632825ff61f5b7263a
    </div>
   )
 }
